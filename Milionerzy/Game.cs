c164e86277ea9cf54a27dbf1298b6c5264afc44b
Bartek <bartek.pozniewski@wp.pl>
2022-10-10 14:38:39 +0200

    Add project files.

---
+++ Milionerzy/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milionerzy
+{
+    internal class Game
+    {
+        public string playerName { get; set; }
+        public List<Question> questions { get; set; }
+        public int roundNumber { get; set; }
+
+        public bool isGameActive { get; set; } = false;
+
+        public enum difficultyLevel
+        {
+            easy,
+            medium,
+            hard
+        }
+        public List<Question> readQuestions(string fileName)
+        {
+            List<Question> questionList = new List<Question>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                int i = 0, questionNumber = -1;
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            questionNumber++;
+                            questionList.Add(new Question());
+                            questionList[questionNumber].numberOfQuestion = questionNumber;
+                            questionList[questionNumber].question = line;
+                            break;
+                        case 1:
+                            questionList[questionNumber].answerA = line;
+                            break;
+                        case 2:
+                            questionList[questionNumber].answerB = line;
+                            break;
+                        case 3:
+                            questionList[questionNumber].answerC = line;
+                            break;
+                        case 4:
+                            questionList[questionNumber].answerD = line;
+                            break;
+                        case 5:
+                            questionList[questionNumber].correctAnswer = line;
+                            break;
+                        default:
+                            break;
+                    }
+                    i++; i = i % 6;
+                    //Console.WriteLine(line);
+                }
+            }
+            return questionList;
+        }
+    }
+}
---
+++ Milionerzy/Program.cs
@@ -0,0 +1,31 @@
+﻿using Milionerzy;
+using System.Text;
+
+Game game = new Game();
+
+Console.WriteLine("Nowa gra");
+Console.WriteLine("Ustawienia");
+Console.WriteLine("Rekordy");
+Console.WriteLine("Wyjście");
+
+game.isGameActive = true;
+game.questions = game.readQuestions("easy.txt"); // List of questions in the game
+
+while (game.isGameActive)
+{
+    Console.WriteLine("Witaj w grze Milionerzy!\n Podaj swój nick: ");
+    game.playerName=Console.ReadLine();
+    Console.WriteLine("Sześć "+game.playerName);
+
+    if (Console.ReadKey().Key == ConsoleKey.X) game.isGameActive = false;
+    Console.WriteLine("Pytanie numer xd");
+    Console.WriteLine(game.questions[0].question);
+    Console.WriteLine(game.questions[0].answerA);
+    Console.WriteLine(game.questions[0].answerB);
+    Console.WriteLine(game.questions[0].answerC);
+    Console.WriteLine(game.questions[0].answerD);
+    
+    
+}
+
+Console.ReadKey();
