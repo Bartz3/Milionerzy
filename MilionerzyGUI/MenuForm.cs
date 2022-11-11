f6f6764445ede7f6db2193aed5bb7c804bbdfb5b
BartekP <bartek.pozniewski@wp.pl>
2022-11-11 17:04:45 +0100

    Added Windows Form to the project

---
+++ MilionerzyGUI/MenuForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilionerzyGUI
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            SoundPlayer intro = new SoundPlayer("intro.wav");
+            intro.Load();
+            intro.Play();
+            InitializeComponent();
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RoundForm roundForm = new RoundForm();
+            roundForm.ShowDialog();
+        }
+    }
+}
